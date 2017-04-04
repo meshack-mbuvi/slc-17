e4d26a07a9b65a42ba2a704d0c3889af80247752
meshack-mbuvi <meshmbuvi@gmail.com>
2017-04-04 09:48:59 +0300

    Create prime_number.py with function to generate prime numbers from 0 to n Made some changes to testcases in prime_number_test.py file

---
+++ prime_number.py
@@ -0,0 +1,36 @@
+def generate_prime_numbers(n):
+	'''
+	This function generates prime numbers from 0 to n.
+	:args:n 
+	:return: primes-a list containing all prime numbers from 0 to n
+	:Author: Meshack Mbuvi
+	:Email:meshmbuvi@gmail.com
+	:Phone:+254719800509
+
+	'''
+	primes=[]
+	if isinstance(n,str):
+		return 'input of type string not allowed'
+	elif n<0:
+		return 'only positive numbers are allowed'
+	elif isinstance(n,float):
+		return 'only whole numbers are allowed'
+	elif type(n)==type(list):
+		return 'inputs of type list is not allowed'
+	elif type(n)==type(dict):
+		return 'dictionary inputs not allowed'
+	else:
+		for number in range(0,n+1):
+			if number>13:
+				if not number%2==0 and not number%3==0 and not number%5==0 and not number%7==0 and not number%11==0 and not number%13==0:
+					primes.append(number)
+			else:
+				if number>1 and number<=3:
+					primes.append(number)
+				elif not number%2==0 and not number%3==0 and number>1:
+					primes.append(number)
+		return primes
+if __name__=='__main__':
+	print generate_prime_numbers(15)
+	print generate_prime_numbers('10')
+	print generate_prime_numbers(200)
--- prime_number_tests.py
+++ prime_number_tests.py
@@ -3,22 +3,22 @@ from prime_number import generate_prime_numbers
 class prime_numbers(unittest.TestCase):
 	#test for non number inputs
 	def test_non_number_inputs(self):
-		self.assertTrue(generate_prime_numbers('123'),msg='input of type string not allowed')
+		self.assertTrue(generate_prime_numbers('123'),'input of type string not allowed')
 	#should not accept negative numbers
 	def test_negative_numbers(self):
-		self.assertTrue(generate_prime_numbers(-1),msg='only positive numbers are allowed')
+		self.assertEqual(generate_prime_numbers(-1),'only positive numbers are allowed')
 	#should not accept lists
 	def test_list_input(self):
-		self.assertTrue(generate_prime_numbers([2]),msg='inputs of type list is not allowed')
+		self.assertEqual(generate_prime_numbers([2]),'inputs of type list is not allowed')
 	#should work on only whole numbers
 	def test_non_whole_numbers(self):
-		self.assertTrue(generate_prime_numbers(1.5),msg='only whole numbers are allowed')
+		self.assertTrue(generate_prime_numbers(1.5),'only whole numbers are allowed')
 	#should not accept dictionary inputs
 	def test_dict_input(self):
-		self.assertEqual(generate_prime_numbers({1:1}),msg='dictionary inputs not allowed')
+		self.assertTrue(generate_prime_numbers({1:1}),'dictionary inputs not allowed')
 	#test that function works correctly
 	def test_it_works_correctly(self):
-		self.assertEqual(generate_prime_numbers(10),msg=[2,3,5,7])
-		self.assertEqual(generate_prime_numbers(5),msg=[2,3,5])
+		self.assertEqual(generate_prime_numbers(10),[2,3,5,7])
+		self.assertEqual(generate_prime_numbers(5),[2,3,5])
 if __name__=='__main__':
 	unittest.main()
